dcf8535cc1a9e02ceec68eaaeeadfd1b42b622ef
shreevhatkar <shreeshamvhatkar@gmail.com>
2026-06-15 12:27:42 +0530

    Increase rate limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,7 +26,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: process.env.NODE_ENV === "production" ? 100 : 5000,
+  max: process.env.NODE_ENV === "production" ? 1000 : 5000,
   standardHeaders: true,
   legacyHeaders: false,
 });
